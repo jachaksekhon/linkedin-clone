8ca8a1afb4c46ee5c31e9edea352ab0b8f43370d
Jachak Sekhon <jachak.sekhon@hotmail.com>
2023-03-06 01:54:49 -0800

    connected firebase, added demo post + enter post

--- src/Feed.js
+++ src/Feed.js
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Feed.css"
 import CreateIcon from '@mui/icons-material/Create';
 import InputOption from "./InputOption"
+import Post from "./Post"
 import ImageIcon from '@mui/icons-material/Image';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+import { db } from "./firebase"
+import firebase from 'firebase/compat/app';
 
 
 function Feed() {
+
+  const [input, setInput] = useState("");
+  const[posts, setPosts] = useState([]);
+
+  //   onSnapshot gives us a 'picture' of the collection of the posts
+  //   everytime a post gets added, it will give us a snapshot
+  //   An open parenthesis in an arrow function implies a return
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot(snapshot => (
+        setPosts(snapshot.docs.map(doc => (
+            {
+                id: doc.id,
+                data: doc.data(),
+            }
+        )))
+    ))
+  }, [])
+
+  const sendPost = e => { /* e is short for "event" */
+    e.preventDefault(); /* prevents the default behaviour of refreshing */
+    db.collection('posts').add({
+        name: "Jachak Sekhon",
+        description: "This is a test",
+        message: input,
+        photoUrl: '',
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    })
+  };
+
   return (
     <div className="feed">
         <div className="feed__inputContainer">
             <div className="feed__input">
                 <CreateIcon />
                 <form>
-                    <input type="text" />
-                    <button type="submit">Send</button>
+                    <input value = {input} onChange={ e=> setInput(e.target.value)} type="text" />
+                    <button onClick={sendPost} type="submit">Send</button>
                 </form>
             </div>
             <div className="feed__inputOptions">
@@ -26,6 +59,9 @@ function Feed() {
                 <InputOption Icon={CalendarViewDayIcon} title="Write Article" color="#7FC15E"/>
             </div>
         </div>
+        {posts.map((post) => (<Post />)) }
+        <Post name="Jachak Sekhon" description="Testing" message="this is actually working"/>
+
     </div>
   )
 }
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBgpj8XQgfElpRq7Xav_oU5POnJbURXV9k",
+    authDomain: "linkedinclone-909c1.firebaseapp.com",
+    projectId: "linkedinclone-909c1",
+    storageBucket: "linkedinclone-909c1.appspot.com",
+    messagingSenderId: "176726410652",
+    appId: "1:176726410652:web:150e8148ad8cc32c29b459"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig); /* this part connects our app to db */
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth } /* since we need this outside of this file we export it */
